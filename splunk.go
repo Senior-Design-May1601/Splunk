ee2f11b86352759a7a38898a3407b3c742d48a97
Crazzytrain36 <osborj1@iastate.edu>
2015-10-17 00:46:32 -0500

    Event Structure
    Built Event structure and some basic error handling. Waits for 5 seconds
    then retry's

--- splunk.go
+++ splunk.go
@@ -6,18 +6,43 @@ import ("net/http"
 		"bytes"
 		"crypto/tls"
 		"encoding/json"
+		"time"
+		"os"
 )
 
 const(
  TOKEN = "5B6AB11C-FB81-4B06-A300-99E31FE3E781"  // Edit to client provided token from Splunk Event Collector
  EVENT = "https://localhost:8088/services/collector" // Change to Splunk server IP/services/collector
 )
+type Http struct{
+	Method string `json:"method"` 
+	Path string `json:"path"`
+	Parameters map[string]string `json:"parameters"`
+}
+
+type Https struct{
+	method string
+	path string
+	parameters map[string]string 
+}
+
+type SSH struct{
+	username string
+	password string
+	key	string
+}
+type Event struct{
+	SourceAddress string `json:"source address"`
+	SourcePort int `json:"source port"`
+	ServiceType string `json:"service type"`
+	SSH *SSH `json:"ssh,omitempty"`
+	Http *Http `json:"http,omitempty"`
+	Https *Https `json:"https,omitempty"`
+}
 
 // Throw error if exception found
-func check(e error){
-	if e!=nil{
-		panic(e)
-	}
+func getDependecies(){
+		
 }
 
 // HttpClient with TLS 
@@ -29,50 +54,51 @@ func httpClient() *http.Client{
         return client
 }
 
-// Add event from string
-func addEvent(_data string) (string,error){
+// Do five attempts, one every 5 seconds. If it still fails write to file
+func cacheEvent(client *http.Client,request *http.Request,data []byte)(*http.Response,error){
+	i:=0
+	var resp *http.Response
+	var err error
+	for(i<5){
+		time.Sleep(5 * time.Second)
+		resp, err = client.Do(request)
+		if(err == nil){
+			break
+		}
+		i++
+	}
+	if(err!=nil){
+		fp,_:= os.OpenFile("cache.txt",os.O_WRONLY|os.O_APPEND,0666)
+		fp.Write(data)
+	}
+	return resp,err
+}
+/*
+* Usage example
+*	m := map[string]string{"username":"user","password":"pass"}
+*	h:=Http{Method:"POST",Path:"index.html",Parameters:m}
+*	conn := Event{SourceAddress: "source",SourcePort: 239,ServiceType: "http",Http: &h,SSH : nil,Https:nil}
+*	b,err:=conn.Send()
+*/
+
+func (e Event) Send() (string,error){
 	client := httpClient()
 	var payload io.Reader
-     
-	data := map[string]string{"event":_data} 
+    data:= map[string] Event{"event":e}  
 	json_data,err := json.Marshal(data)
-	check(err)
 	
+	//os.Stdout.Write(json_data)
 	payload = bytes.NewReader(json_data)
 	request, err:= http.NewRequest("POST", EVENT, payload)
-	check(err)
+	
 	
 	request.Header.Add("Authorization", fmt.Sprintf("Splunk %s", TOKEN))
 	resp,err:=client.Do(request)
-	response,err:= ioutil.ReadAll(resp.Body)
-	
-	return string(response),err
-}
-
-//Create event from a file
-func addEventFromFile(filePath string,sourceType string) (string,error){
-	_file, err := ioutil.ReadFile(filePath)
-	check(err)
-	content:=string(_file)
-	client := httpClient()
-	var data map[string]string
-	
-	if len(sourceType)>0{
-		data = map[string]string{"event":content,"sourcetype":sourceType}
+	if(err!=nil){
+		go cacheEvent(client,request,json_data)
+		return "",err
 	}else{
-		data = map[string]string{"event":content}
+		response,err:= ioutil.ReadAll(resp.Body)
+		return string(response),err
 	}
-	
-	json_data,err := json.Marshal(data)
-	check(err)
-	
-	payload := bytes.NewReader(json_data)
-	request, err:= http.NewRequest("POST", EVENT, payload)
-	check(err)
-	
-	request.Header.Add("Authorization", fmt.Sprintf("Splunk %s", TOKEN))
-	resp,err:=client.Do(request)
-	response,err:= ioutil.ReadAll(resp.Body)
-	
-	return string(response),err
 }
